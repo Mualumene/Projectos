b281cada71967eff25fc87c3b2a8254f15edd2a0
Renato <jrenato@intersystem.eti.br>
2015-10-02 14:51:47 -0300

    Criado o Domínio e o Projeto de Testes

---
+++ EscolaModelo/src/EscolaModelo.Domain/Entities/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EscolaModelo.Domain.Entities
+{
+    public class Aluno
+    {
+        public Aluno()
+        {
+            AlunoId = Guid.NewGuid();
+        }
+
+        public Guid AlunoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public int ObterIdade(Aluno aluno)
+        {
+            var idade = DateTime.Now.Year - aluno.DataNascimento.Year;
+            
+
+            return idade;
+        }
+    }
+}
---
+++ EscolaModelo/src/EscolaModelo.Tests/Testes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EscolaModelo.Domain.Entities;
+
+namespace EscolaModelo.Tests
+{
+    [TestClass]
+    public class Testes
+    {
+        [TestMethod]
+        public void TestaObterIdade()
+        {
+            // Arranjo
+            var aluno = new Aluno();
+            aluno.Nome = "Renato";
+            aluno.DataNascimento = DateTime.Parse("27/10/1972");
+
+            // Ação
+            var resultado = aluno.ObterIdade(aluno);
+
+            // Assert
+            Assert.AreEqual(42, resultado);
+
+        }
+    }
+}
